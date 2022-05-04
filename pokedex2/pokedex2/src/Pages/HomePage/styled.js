e5a56ae7b93af4155ca8abce381b7a623ad5da55
yasminconc <yasminsilvaconc@gmail.com>
2022-05-04 15:15:41 -0300

    atualizado

--- pokedex2/pokedex2/src/Pages/HomePage/styled.js
+++ pokedex2/pokedex2/src/Pages/HomePage/styled.js
@@ -1,13 +1,39 @@
 import styled from "@emotion/styled"
 
+export const Header = styled.div`
+display: grid;
+grid-template-columns: 20% 80%;
+background-color: red;
+`
 
 export const ContainerButton = styled.div`
-
-display: grid;
-grid-auto-flow: column;
-gap: 6px;
+margin: 30px;
 
 a{
-    text-decoration: none;
+     text-decoration: none;
 }
 `
+
+export const ConatinerImg = styled.div`
+margin-left: 30%;
+`
+
+export const Main = styled.main`
+display: flex;
+
+`
+
+// export const ContainerPai= styled.div`
+
+// background-color: aliceblue;
+
+// `
+
+// export const ContainerButton = styled.div`
+
+// a{
+//     text-decoration: none;
+// }
+
+
+// `
